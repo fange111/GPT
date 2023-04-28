7ecc0ff4a6eca3b6f82040d98f11838b61d143ac
PowerBelief <200893453@qq.com>
2023-04-28 22:38:27 +0800

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
+      <div>
+        打开官方网站{" "}
+        <a
+          href="https://beta.openai.com/account"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          OpenAI
+        </a>
+      </div>
+
+    </footer>
+  );
+}
